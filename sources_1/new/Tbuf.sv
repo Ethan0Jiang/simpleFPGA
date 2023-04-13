34f1915ee5db62192a7bd5dac46a7b7a58637043
Ethan <zhixij@uw.edu>
2023-04-13 15:40:36 -0700

    first

---
+++ sources_1/new/Tbuf.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/13/2023 12:38:25 PM
+// Design Name: 
+// Module Name: Tbuf
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Tbuf(
+
+    );
+endmodule
